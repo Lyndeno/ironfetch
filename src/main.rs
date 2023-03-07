8c4f04eb3e464ac366b2e193cd6ac1ea6f1297ba
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-07 13:01:42 -0700

    Implement simple kernel information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use nix::sys::utsname::{UtsName, uname};
 fn main() {
     println!("Hello, world!");
+    let info = uname().unwrap();
+    println!("{} {}", info.sysname().to_str().unwrap(), info.release().to_str().unwrap());
 }
