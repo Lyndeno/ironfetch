b587de704cdefc26fff3b64f2736db927cad7aeb
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-10 13:09:59 -0700

    Allow choosing between megabytes and gigabytes

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,41 @@ use crate::cpu::Cpu;
 mod mem;
 use crate::mem::Memory;
 
+use clap::Parser;
+
 use os_release;
 
+/// Simple fetching program
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Whether to use gigabytes
+    #[arg(short, long)]
+    gigabyte: bool,
+    
+    /// Whether to use megabytes
+    #[arg(short, long)]
+    megabyte: bool,
+}
+
 fn main() {
+    let args = Args::parse();
     let kernel_info = Kernel::new();
     let cpu_info = Cpu::new();
+    
     let mem_info = Memory::new();
+    let mem_display = match args.gigabyte {
+        true => mem_info.display_gb(),
+        false => match args.megabyte {
+            true => mem_info.display_mb(),
+            false => mem_info.display_gb(),
+        }
+    };
+
     let os = os_release::OsRelease::new().unwrap();
     println!("Distro: {}", os.pretty_name);
     println!("Kernel: {}", kernel_info);
     println!("CPU: {}", cpu_info);
-    println!("Memory: {}", mem_info);
+    println!("Memory: {}", mem_display);
     println!("OS: {}", os.pretty_name);
 }
