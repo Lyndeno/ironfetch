3de116bd4b7058fefe94d90fa78667174c7d1b34
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-14 12:09:43 -0600

    Add formatter hooks

--- flake.nix
+++ flake.nix
@@ -4,9 +4,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     naersk.url = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
+
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+    pre-commit-hooks-nix,
+  }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages."${system}";
       naersk-lib = naersk.lib."${system}";
@@ -22,9 +32,24 @@
       };
       apps.default = apps.ironfetch;
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [ rustc cargo rustfmt ];
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      };
+      formatter = pkgs.alejandra;
+
+      devShells.default = let
+        pre-commit-format = pre-commit-hooks-nix.lib.${system}.run {
+          src = ./.;
+
+          hooks = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [rustc cargo rustfmt];
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          shellHook = ''
+            ${pre-commit-format.shellHook}
+          '';
+        };
     });
 }
