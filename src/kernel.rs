d1b454fdc84c0e7bb0681ac8b81e008b272c01b1
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-14 21:04:36 -0600

    Refactor kernel info

--- src/kernel.rs
+++ src/kernel.rs
@@ -6,9 +6,9 @@ type Result<T> = std::result::Result<T, KernelError>;
 #[derive(Debug, Clone)]
 pub struct KernelError;
 pub struct Kernel {
-    release: Option<String>,
-    architecture: Option<String>,
-    name: Option<String>,
+    release: String,
+    architecture: String,
+    name: String,
 }
 
 impl std::fmt::Display for KernelError {
@@ -22,41 +22,22 @@ impl Kernel {
         let info: UtsName = uname().map_err(|_| KernelError)?;
         Ok(Self {
             // TODO: Error correction
-            release: Self::os_str_to_string(info.release()),
-            architecture: Self::os_str_to_string(info.machine()),
-            name: Self::os_str_to_string(info.sysname()),
+            release: Self::os_str_to_string(info.release())?,
+            architecture: Self::os_str_to_string(info.machine())?,
+            name: Self::os_str_to_string(info.sysname())?,
         })
     }
 
-    fn os_str_to_string(v: &OsStr) -> Option<String> {
+    fn os_str_to_string(v: &OsStr) -> Result<String> {
         match v.to_str() {
-            Some(s) => Some(String::from(s)),
-            None => None,
+            Some(s) => Ok(String::from(s)),
+            None => Err(KernelError),
         }
     }
 }
 
 impl std::fmt::Display for Kernel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut s = String::new();
-        let arr = [
-            self.name.as_ref(),
-            self.release.as_ref(),
-            self.architecture.as_ref(),
-        ];
-        let mut index = 0;
-        for item in arr {
-            match item {
-                Some(s2) => {
-                    if index > 0 {
-                        s.push_str(" ")
-                    };
-                    s.push_str(s2.to_string().as_str());
-                    index = index + 1;
-                }
-                None => {}
-            };
-        }
-        write!(f, "{}", s)
+        write!(f, "{} {} {}", self.name, self.release, self.architecture)
     }
 }
