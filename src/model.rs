d316892ecb48cd6927a647e7515ba7db0134e735
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-14 20:41:49 -0600

    Show more verbose model information

--- src/model.rs
+++ src/model.rs
@@ -2,19 +2,35 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
-pub struct Model(String);
+pub struct Model {
+    product_name: String,
+    board_vendor: String,
+    board_name: String,
+}
 
 impl Model {
     pub fn new() -> Result<Self, std::io::Error> {
-        let f = File::open("/sys/devices/virtual/dmi/id/product_name")?;
-        let mut s = String::new();
-        BufReader::new(f).read_line(&mut s)?;
-        Ok(Model(s.replace("\n", "")))
+        Ok(Self {
+            product_name: read_product_info("/sys/devices/virtual/dmi/id/product_name")?,
+            board_vendor: read_product_info("/sys/devices/virtual/dmi/id/board_vendor")?,
+            board_name: read_product_info("/sys/devices/virtual/dmi/id/board_name")?,
+        })
     }
+}
+
+fn read_product_info(path: &str) -> Result<String, std::io::Error> {
+    let f = File::open(path)?;
+    let mut s = String::new();
+    BufReader::new(f).read_line(&mut s)?;
+    Ok(s.replace("\n", ""))
 }
 
 impl std::fmt::Display for Model {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        write!(
+            f,
+            "{} {} {}",
+            self.board_vendor, self.product_name, self.board_name
+        )
     }
 }
