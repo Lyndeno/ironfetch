fc572fa95d661f3d8d4fa767ee37fabd1233f90c
Lyndon Sanche <lsanche@lyndeno.ca>
2023-07-03 12:18:54 -0600

    Clean up memory code

--- src/mem.rs
+++ src/mem.rs
@@ -1,5 +1,3 @@
-use std::ops::{Add, Sub};
-
 use std::fmt::Write;
 
 use crate::fetcherror::FetchError;
@@ -7,12 +5,14 @@ use crate::fetchitem::FetchItem;
 use crate::memunit::MemUnits;
 use crate::proc::proc_parse;
 
+use measurements::data::Data;
+
 #[derive(Copy, Clone)]
 pub struct MemBytes(u64);
 pub struct Memory {
-    total: MemBytes,
+    total: Data,
     //free: MemBytes,
-    avail: MemBytes,
+    avail: Data,
     //buffers: MemBytes,
     //cached: MemBytes,
     //swap_total: MemBytes,
@@ -24,21 +24,29 @@ impl Memory {
     pub fn new(unit: Option<MemUnits>) -> Result<Self, FetchError> {
         let path = "/proc/meminfo";
         Ok(Self {
-            total: MemBytes::from_proc(proc_parse(path, "MemTotal")?),
-            avail: MemBytes::from_proc(proc_parse(path, "MemAvailable")?),
+            total: mem_from_proc(proc_parse(path, "MemTotal")?),
+            avail: mem_from_proc(proc_parse(path, "MemAvailable")?),
             display_unit: unit.unwrap_or(MemUnits::GB),
         })
     }
 
-    pub fn used(&self) -> MemBytes {
+    pub fn used(&self) -> Data {
         self.total - self.avail
     }
 
     pub fn display_gb(&self) -> String {
-        self.display_unit(self.used().as_gb(), self.total.as_gb(), "GiB")
+        self.display_unit(
+            self.used().as_gibioctets(),
+            self.total.as_gibioctets(),
+            "GiB",
+        )
     }
     pub fn display_mb(&self) -> String {
-        self.display_unit(self.used().as_mb(), self.total.as_mb(), "MiB")
+        self.display_unit(
+            self.used().as_mebioctets(),
+            self.total.as_mebioctets(),
+            "MiB",
+        )
     }
 
     fn display_unit(&self, used: f64, total: f64, unit: &str) -> String {
@@ -55,36 +63,8 @@ impl Memory {
     }
 }
 
-impl MemBytes {
-    fn as_gb(&self) -> f64 {
-        self.as_mb() / (1024_f64)
-    }
-    fn as_mb(&self) -> f64 {
-        (self.0 as f64) / (1024f64)
-    }
-    fn from_proc(line: String) -> Self {
-        Self::from(line.replace("kB", "").trim().parse::<u64>().unwrap())
-    }
-}
-
-impl Add for MemBytes {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        Self(self.0 + rhs.0)
-    }
-}
-
-impl Sub for MemBytes {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self {
-        Self(self.0 - rhs.0)
-    }
-}
-
-impl From<u64> for MemBytes {
-    fn from(m: u64) -> MemBytes {
-        MemBytes(m)
-    }
+fn mem_from_proc(line: String) -> Data {
+    Data::from_kibioctets(line.replace("kB", "").trim().parse::<f64>().unwrap())
 }
 
 impl std::fmt::Display for Memory {
@@ -96,16 +76,5 @@ impl std::fmt::Display for Memory {
 impl FetchItem for Memory {
     fn name(&self) -> String {
         String::from("Memory")
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::MemBytes;
-
-    #[test]
-    fn byte_math() {
-        let m = MemBytes(1048576);
-        assert_eq!(m.as_mb() / 1024_f64, m.as_gb());
     }
 }
