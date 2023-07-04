dd2501376e9b75fbad1c15637cc5a1bf330bbbed
Lyndon Sanche <lsanche@lyndeno.ca>
2023-07-04 10:33:57 -0600

    Refactor cpu module

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,52 +3,40 @@ use measurements::frequency::Frequency;
 use procfs::CpuInfo;
 
 pub struct Cpu {
-    cpu: CpuInfo,
+    core_count: usize,
+    model: String,
+    freq: Frequency,
 }
 
 impl Cpu {
     pub fn new() -> Result<Self, FetchError> {
-        Ok(Self {
-            cpu: CpuInfo::new().unwrap(),
-        })
-    }
+        let cpu = CpuInfo::new().unwrap();
+        let core_count = cpu.num_cores();
+        let model = cpu.model_name(0).unwrap_or("").to_string();
 
-    pub fn core_count(&self) -> usize {
-        self.cpu.num_cores()
-    }
-
-    pub fn model(&self) -> String {
-        self.cpu.model_name(0).unwrap_or("").to_string()
-    }
-
-    pub fn speed(&self) -> Frequency {
         let mut sum = Frequency::from_hertz(0f64);
-        for core in 0..self.core_count() {
-            sum = sum + self.core_speed(core);
+        for cpu_num in 0..core_count {
+            sum = sum
+                + Frequency::from_megahertz(
+                    cpu.get_field(cpu_num, "cpu MHz")
+                        .unwrap()
+                        .parse::<f64>()
+                        .unwrap(),
+                );
         }
-        sum / self.core_count() as f64
-    }
+        let freq = sum / core_count as f64;
 
-    fn core_speed(&self, cpu_num: usize) -> Frequency {
-        Frequency::from_megahertz(
-            self.cpu
-                .get_field(cpu_num, "cpu MHz")
-                .unwrap()
-                .parse::<f64>()
-                .unwrap(),
-        )
+        Ok(Self {
+            core_count,
+            model,
+            freq,
+        })
     }
 }
 
 impl std::fmt::Display for Cpu {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{} ({}) @ {:.3}",
-            self.model(),
-            self.core_count(),
-            self.speed()
-        )
+        write!(f, "{} ({}) @ {:.3}", self.model, self.core_count, self.freq)
     }
 }
 
