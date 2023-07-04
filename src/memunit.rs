e9e0431f71fc0a955e7ca8039049be9b64843d88
Lyndon Sanche <lsanche@lyndeno.ca>
2023-07-04 11:46:24 -0600

    Clean up memunit code

--- src/memunit.rs
+++ src/memunit.rs
@@ -1,20 +1,9 @@
 use clap::ValueEnum;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, ValueEnum)]
 pub enum MemUnits {
+    /// Megabytes
     MB,
+    /// Gigabytes
     GB,
-}
-
-impl ValueEnum for MemUnits {
-    fn value_variants<'a>() -> &'a [Self] {
-        &[MemUnits::GB, MemUnits::MB]
-    }
-
-    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
-        Some(match self {
-            MemUnits::GB => clap::builder::PossibleValue::new("gb").help("Gigabytes"),
-            MemUnits::MB => clap::builder::PossibleValue::new("mb").help("Megabytes"),
-        })
-    }
 }
