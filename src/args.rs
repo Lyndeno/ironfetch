8959f2e88d77b1ca70b70d513aaba15dc8613000
Lyndon Sanche <lsanche@lyndeno.ca>
2024-12-17 14:59:44 -0700

    Remove unused args

--- src/args.rs
+++ src/args.rs
@@ -9,13 +9,6 @@ pub struct Args {
     #[arg(long, short, value_enum)]
     pub memory_unit: Option<MemUnits>,
 
-    /// Print errors and other extra information
-    #[arg(long, short)]
-    pub debug: bool,
-
-    #[arg(long, short, value_enum)]
-    pub smbios_path: Option<String>,
-
     /// Number of colours to show
     #[arg(long, default_value = "16")]
     pub colours: usize,
