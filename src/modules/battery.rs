31802162128fe4b901d7ecec3999f7c34f9d2605
Lyndon Sanche <lsanche@lyndeno.ca>
2025-04-14 12:30:28 -0600

    modules/battery: Switch to own upower impl

--- src/modules/battery.rs
+++ src/modules/battery.rs
@@ -2,7 +2,9 @@ use std::fmt::Display;
 
 use derive_more::Display;
 use serde::{Deserialize, Serialize};
-use upower_dbus::{BatteryState, UPowerProxy};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+use zbus::{proxy, zvariant::OwnedValue};
 
 use crate::{fetch::Fetch, Result};
 
@@ -20,6 +22,52 @@ impl From<BatteryState> for State {
     fn from(value: BatteryState) -> Self {
         Self(value)
     }
+}
+
+#[derive(Copy, Clone, Debug, Serialize_repr, Deserialize_repr, OwnedValue, PartialEq, Eq)]
+#[repr(u32)]
+enum BatteryState {
+    Unknown = 0,
+    Charging = 1,
+    Discharging = 2,
+    Empty = 3,
+    FullyCharged = 4,
+    PendingCharge = 5,
+    PendingDischarge = 6,
+}
+
+#[derive(Copy, Clone, Debug, Serialize_repr, Deserialize_repr, OwnedValue, PartialEq, Eq)]
+#[repr(u32)]
+enum BatteryLevel {
+    Unknown = 0,
+    None = 1,
+    Low = 3,
+    Critical = 4,
+    Normal = 6,
+    High = 7,
+    Full = 8,
+}
+
+#[proxy(interface = "org.freedesktop.UPower", assume_defaults = true)]
+trait UPower {
+    #[zbus(object = "Device")]
+    fn get_display_device(&self);
+}
+
+#[proxy(
+    interface = "org.freedesktop.UPower.Device",
+    default_service = "org.freedesktop.UPower",
+    assume_defaults = false
+)]
+trait Device {
+    #[zbus(property)]
+    fn battery_level(&self) -> zbus::Result<BatteryLevel>;
+
+    #[zbus(property)]
+    fn percentage(&self) -> zbus::Result<f64>;
+
+    #[zbus(property)]
+    fn state(&self) -> zbus::Result<BatteryState>;
 }
 
 #[allow(clippy::enum_glob_use)]
